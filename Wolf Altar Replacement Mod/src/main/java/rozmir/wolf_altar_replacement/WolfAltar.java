ff67cd672ea9669c80c0403c224712dc2af837af
Rozmir <120696188+Rozmir-Rohi@users.noreply.github.com>
2024-02-25 23:16:38 +1000

    Moved the Witchery Wolf Altar Replacement mod to its own repository
    New repository for the Witchery Wolf Altar Replacement mod: https://github.com/Rozmir-Rohi/witchery-wolf-altar-replacement

--- Wolf Altar Replacement Mod/src/main/java/rozmir/wolf_altar_replacement/WolfAltar.java
+++
@@ -1,8 +0,0 @@
-package rozmir.wolf_altar_replacement;
-
-import cpw.mods.fml.common.DummyModContainer;
-import cpw.mods.fml.common.Mod;
-
-
-@Mod (modid = "WolfAltarReplacement", name = "Wolf Altar Replacement", version = "1.0")
-public class WolfAltar {}
